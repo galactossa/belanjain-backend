61efd6d0d636003cab21266685e5384d042b75cd
Tora Eldoardo Eko Putra <toraeld8@gmail.com>
2026-06-18 07:57:28 +0700

    update db.js

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-// 🔥 PAKAI DATABASE_URL DARI ENVIRONMENT (RAILWAY)
+// 🔥 PAKAI DATABASE_URL DARI ENVIRONMENT
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // PENTING UNTUK RAILWAY
+    rejectUnauthorized: false,
   },
 });
 
